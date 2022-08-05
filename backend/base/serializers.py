6dd1c21f53b214ade69acfe81ec895384b12284a
Bart <bartlomiej.filipowicz97@gmail.com>
2022-08-05 12:53:48 +0200

    Add Order View & URL
    Created the order view where the Order model, ShippingAddress model and
    OrderItem models are created, Order to OrderItem relationship is set and
    stock is updated. Created URL for that view.

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -3,7 +3,7 @@ import imp
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, Order, OrderItem, ShippingAddress
 
 # I create a serializer for every single model that I want to return, a serializer is going to wrap my model
 # and turn that model into a JSON format
@@ -48,3 +48,45 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orders = serializers.SerializerMethodField(read_only=True) 
+    shippingAddress = serializers.SerializerMethodField(read_only=True)  
+    user = serializers.SerializerMethodField(read_only=True) 
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orders(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+        except:
+            address = False
+
+        return address
+
+    def get_user(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
---
+++ backend/base/views/order_views.py
@@ -0,0 +1,72 @@
+from email import message
+from email.mime import image
+from itertools import product
+from math import prod
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from base.models import Product, Order, OrderItem, ShippingAddress
+
+
+from rest_framework import status
+
+from base.serializers import ProductSerializer, OrderSerializer
+
+from .. import serializers
+
+@api_view(['POST'])
+@permission_classes(['IsAuthenticated'])
+def addOrderItems(request):
+
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # 1. Create Order model
+
+        order = Order.objects.create(
+            user = user,
+            paymentMethod = data['paymentMethod'],
+            taxPrice = data['taxPrice'],
+            shippingPrice = data['shippingPrice'],
+            totalPrice = data['totalPrice']
+        )
+
+        # 2. Create ShippingAddress model
+
+        shipping = ShippingAddress.objects.create(
+            order = order,
+            address = data['shippingAddress']['address'],
+            city = data['shippingAddress']['city'],
+            postalCode = data['shippingAddress']['postalCode'],
+            country = data['shippingAddress']['country'],
+
+        )
+
+        # 3. Create OrderItem models and set Order to OrderItem relationship
+
+        for i in orderItems:
+            product = Product.objects.get(_id = i['product']) # 'product' is an id
+
+            item = OrderItem.objects.create(
+                product = product,
+                order = order,
+                name = product.name,
+                qty = i['qty'],
+                price = i['price'],
+                image = product.image.url,
+            )
+
+            # 4. Update stock
+
+            product.countInStock -= item.qty
+            product.save()
+
+    serializer = OrderSerializer(order, many=False)
+
+    return Response(serializer.data)
