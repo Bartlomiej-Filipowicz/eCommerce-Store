6c7f6e9dcfab7f8a55521db6cc7c442dcfbde9a1
Bart <bartlomiej.filipowicz97@gmail.com>
2022-08-08 17:46:51 +0200

    Create Order Details Reducer & Action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,7 +2,11 @@ import axios from 'axios'
 import { 
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL
+    ORDER_CREATE_FAIL,
+
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants'
 
 import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
@@ -51,6 +55,54 @@ export const createOrder = (order) => async (dispatch, getState) => {
     {
         dispatch({
             type: ORDER_CREATE_FAIL,
+            payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message,
+        })
+    }
+
+}
+
+
+
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+
+    try
+    {
+        dispatch({
+            type: ORDER_DETAILS_REQUEST
+        })
+
+        const {
+            userLogin: { userInfo }, // I get information who's logged in
+        } = getState()              // getState() takes a state of an entire application
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}` // the route to a user profile page is protected that's why
+                                                         //  authorization is needed
+            }
+        }
+
+        // it sends a username and a password and gets in return the JWT token
+        const {data} = await axios.get(
+            `/api/orders/${id}/`,
+            config
+        )
+
+        dispatch({
+            type: ORDER_DETAILS_SUCCESS,
+            payload: data 
+        })
+
+
+    }
+    catch (error)
+    {
+        dispatch({
+            type: ORDER_DETAILS_FAIL,
             payload: error.response && error.response.data.detail
             ? error.response.data.detail
             : error.message,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
-import { orderCreateReducer } from './reducers/orderReducers'
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer, // productList is a state
@@ -18,6 +18,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 // if there are no products in a cart, return an empty array
