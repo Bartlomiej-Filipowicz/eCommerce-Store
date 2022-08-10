da45d5482d0f4a5f6619a5bd42d9bbbf901cb9b5
Bart <bartlomiej.filipowicz97@gmail.com>
2022-08-10 11:04:07 +0200

    Add Get User By Id & Update User Endpoints

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name='users-profile'),
     path('profile/update/', views.updateUserProfile, name='user-profile-update'),
     path('', views.getUsers, name='users'),
+
+    path('<str:pk>/', views.getUserById, name='user'),
+
+    path('update/<str:pk>/', views.updateUser, name='user-update'),
+
     path('delete/<str:pk>/', views.deleteUser, name='user-delete'),
 ]
