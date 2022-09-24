62a8be09196286b9c41969db5014a53d14aa1ca5
Bart <bartlomiej.filipowicz97@gmail.com>
2022-09-24 18:23:46 +0200

    Move frontend to the root directory

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
+        "DIRS": [os.path.join(BASE_DIR, "../frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -173,7 +173,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 MEDIA_URL = "/images/"
 
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "../frontend/build/static"]
 
 MEDIA_ROOT = "static/images"
 
